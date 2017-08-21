2a53874c0258cc9ab902bcb754fe1f9b31292bec
hunter <hunter@hunter-ThinkPad-X230-Tablet>
2017-08-21 13:00:45 +0800

    init

--- lottery/lottery-skeleton/src/main/java/com/codedak/lottery/service/IMemberService.java
+++ lottery/lottery-skeleton/src/main/java/com/codedak/lottery/service/IMemberService.java
@@ -1,7 +1,9 @@
 package com.codedak.lottery.service;
 
+import com.codedak.hessian.spring.annotation.HessianClient;
 import com.codedak.lottery.entity.Member;
 
+@HessianClient(name="lottery-member-service",linkUrl="lottery-member-service")
 public interface IMemberService {
 	   public void register(Member member);
 	   
